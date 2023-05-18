681189fce7026a9d1c337ccfee9d536230a639e5
Edo88889 <132383556+Edo88889@users.noreply.github.com>
2023-05-18 19:14:20 +0300

    save

--- src/Cocktails/Kofi.js
+++ src/Cocktails/Kofi.js
@@ -1,30 +1,38 @@
 import {useState, useEffect} from "react";
-
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+import './Index.css';
 function Kofi() {
     const [data, setData] = useState([])
-        useEffect(() => {
+    const [values, setValues] = useState('')
+    console.log(values)
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${values}`
+    useEffect(() => {
         fetch(url)
-        .then(response => response.json())
+            .then(response => response.json())
             .then(result => setData(result.drinks))
-        .catch(error => console.log(error));
-            console.log(data)
-        }, []);
-    return(
-        <div className='cocktails'>
-    {data.map((e) => (
-<>
-    <a href='#' onClick={(el)=>{
-        el.preventDefault()
-        alert(e.strGlass)
-    }}>DETAILS</a>
-    <img src={e.strDrinkThumb} alt='foto' className='foto'/>
-    <p>{e.strDrink}</p>
-    <p>{e.strGlass}</p>
-    <p>{e.strAlcoholic}</p>
-</>
-        ))}
-        </div>
-)
+            .catch(error => console.log(error));
+    }, [url]);
+    return (
+        <>
+            <input type='text' value={values} className='value' placeholder='name Cocktails' onChange={(e) => {
+                setValues(e.target.value)
+            }}/>
+            <h1 className='title'>Cocktails</h1>
+            <div className='cocktails'>
+                {data?.map(({strDrink,strGlass,strAlcoholic,strDrinkThumb,idDrink}) => (
+                    <div key={idDrink} className='cards'>
+                        <img src={strDrinkThumb} alt='foto' className='foto'/>
+                        <p className='p'>{strDrink}</p>
+                        <p className='p'>{strGlass}</p>
+                        <p className='p'>{strAlcoholic}</p>
+                       <button className='href' onClick={()=>{
+                           localStorage.setItem(idDrink,strDrink)
+                           setData(data.filter((e) => e.idDrink != idDrink))
+                       }}>DETAILS</button>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
+
 export default Kofi
